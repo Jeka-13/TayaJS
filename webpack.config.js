d2851a6379cc15735143c7399b3ee86741657197
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-03-16 06:02:54 +0200

    Added support for dev and prod mode, css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
 module.exports = {
     context: path.join(__dirname, 'src'),
     mode: 'development',
@@ -13,12 +16,24 @@ module.exports = {
     },
     devtool: 'eval',
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev
     },
     plugins: [
         new htmlWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
